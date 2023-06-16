cffa44a9a4e4f6e5318dee2a26c4869f64b7ab67
brosenmoller <rosenmollerben@gmail.com>
2023-06-16 15:26:04 +0200

    Small Fixes

---
+++ Assets/02_GeneralScripts/ObjectiveMarker.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectiveMarker : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Image marker;
+    [SerializeField] private Transform target;
+}
